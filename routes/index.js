8a4ce8c80ad6b3f61318c9c7d368d72e7ae603f7
brockowen <brockowen91@gmail.com>
2017-04-24 18:28:15 -0500

    Add existing file

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/services', function(req, res){
+router.get('/services', function(req, res, next){
 	res.render('services');
+});
+router.get('/services/industrialrehab', function(req, res){
+	res.render('industrialrehab');
 });
 module.exports = router;
