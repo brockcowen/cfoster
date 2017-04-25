0865759a9438be346b326053d59c214e073df25d
brockowen <brockowen91@gmail.com>
2017-04-25 12:52:44 -0500

    Add existing file

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require('firebase')
 
+
+var fbconfig = {
+    apiKey: "[APIKey]",
+    authDomain: "[authDomain]",
+    databaseURL: "[databaseURL]",
+    projectId: "[projectId]",
+    storageBucket: "[storageBucket]",
+    messagingSenderId: "[messagingSenderId]"
+};
+firebase.initializeApp(fbconfig)
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+	var database = firebase.database().ref('messages');
   res.send('respond with a resource');
+  var messagesRef = database;
+  messagesRef.on('value', function(snapshot){
+  	console.log(snapshot.val());
+  })
+  
 });
 
 module.exports = router;
